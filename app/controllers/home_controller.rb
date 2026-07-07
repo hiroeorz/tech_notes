f20a86f1a8298f82b0ea486bd0bb6a9efd5696f3
Shin Hiroe <hiroe.orz@gmail.com>
2026-07-07 23:25:02 +0900

    トップページの実験ログ表示件数を3件から21件に拡大

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @latest_posts = Post.publicly_visible.article.includes(:category, :tags).recent.limit(5)
-    @experiments = Post.publicly_visible.experiment.includes(:tags).recent.limit(3)
+    @experiments = Post.publicly_visible.experiment.includes(:tags).recent.limit(21)
     @daily_log = Post.publicly_visible.experiment.recent.first
     @top_filter_categories = Category.where(slug: %w[infrastructure aws automation programming security ai-development]).index_by(&:slug)
     load_sidebar
